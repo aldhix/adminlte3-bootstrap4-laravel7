95a737d96a82fc986345e894a8e2d67fe7cb8fb6
aldhix <aldhi.x@gmail.com>
2020-07-01 22:10:13 +0700

    Update about.blade.php

--- resources/views/admin/about.blade.php
+++ resources/views/admin/about.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
 	<div class="col">
 		<p>
-			<strong>Simple</strong> adalah aplikasi Sistem Manajemen Penjualan dan Pembelian.
+			<strong>AdminLTE 3 Bootstrap 4.5 Laravel 7</strong> Paket component AdminLTE yang di conversi kedalam bentuk blade Laravel. Github : <a href="https://github.com/aldhix/adminlte3-bootstrap4-laravel7">https://github.com/aldhix/adminlte3-bootstrap4-laravel7</a>
 		</p>
 	</div>
 </div>
